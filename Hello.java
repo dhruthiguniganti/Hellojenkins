feb5675556663cc36af9ea42590252ccdd573248
dhruthiguniganti <dhruthiguniganti@gmail.com>
2026-08-19 10:51:08 +0530

    Update Hello World message

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Welcome to Jenkins CI/CD Demo");
+        System.out.println("Hello World");
     }
 }
